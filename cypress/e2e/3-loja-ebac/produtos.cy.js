d4251709b4a38bdbeb09b31573b21a7dc97728bb
Ian Silvério <silverio.ian@gmail.com>
2024-05-13 12:57:00 -0300

    testes busca produtos

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,18 +1,28 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Ajax Full-Zip Sweatshirt')
-            .click()
-        cy.get('#tab-title-description > a').should('exist')
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
+        cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+    });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Apollo Running Short'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
+    });
+
+    it('Deve visitar a página do produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
