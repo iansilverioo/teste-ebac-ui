1f7c8316b204664e90111f5aeb8a006fc4619461
Ian Silvério <silverio.ian@gmail.com>
2024-05-13 14:13:03 -0300

    atualização testes produtos

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -12,17 +12,32 @@ describe('Funcionalidade: Produtos', () => {
         cy.get('#tab-title-description > a').should('contain' , 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Apollo Running Short'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain' , produto)
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        let produto = 'Aether Gym Pant'
+        produtosPage.visitarProduto('aether gym pant')
+        cy.get('.product_title').should('contain' , produto)
     });
 
     it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 2
+        produtosPage.buscarProduto('Ariel Roll Sleeve Sweatshirt')
+        produtosPage.addProdutoCarrinho('M', 'Purple', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+    });
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(dados[0].tamanho, 
+                dados[0].cor, 
+                dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        })
         
     });
 });
